23e41c0bbb4390ed0abba2fc87803fb895a9b6b8
TamirlanNurbulatuly <t.nurbulatuly@gmail.com>
2023-03-30 03:56:44 +0600

    5 def returning nothing and urls to them

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from api.views import *
+
 
 urlpatterns = [
-
+    path('products/', products),
+    path('products/<int:id>/', products_by_id),
+    path('categories/', categories),
+    path('categories/<int:id>/', categories_by_id),
+    path('categories/<int:id>/products/', products_by_category_id),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def products(request):
+    return None
+
+
+def products_by_id(request, product_id):
+    return None
+
+
+def categories(request):
+    return None
+
+
+def categories_by_id(request, category_id):
+    return None
+
+
+def products_by_category_id(request):
+    return None
